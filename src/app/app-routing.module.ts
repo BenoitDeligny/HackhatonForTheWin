4011fcf33e7b3960d9c608e5856e962231686046
Fabien <jouanneau.fab@gmail.com>
2020-04-29 23:56:05 +0200

    feat(app-routing): Create root to journey

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { JourneyComponent } from './pages/journey/journey.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'journey', component: JourneyComponent},
+  {path: '', component: JourneyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
