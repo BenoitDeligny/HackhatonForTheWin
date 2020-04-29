931cdbeff048947beb68ca6ce14355c6ede5cf47
Couettou <delignyb.pro@gmail.com>
2020-04-29 18:09:37 +0200

    affichage des éléments dans le template

--- src/app/pages/pimp-my-couch/pimp-my-couch.component.ts
+++ src/app/pages/pimp-my-couch/pimp-my-couch.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PimpMyCouchComponent implements OnInit {
 
+  couchBodies = [
+    'assets/images/tissuCouch.png',
+    'assets/images/cuirCouch.png'
+  ];
+
+  engines = [
+    'assets/images/fastEngine.png',
+    'assets/images/powerfullEngine.png'
+  ];
+
+  wheels = [
+    'assets/images/fastWheel.png',
+    'assets/images/fireWheel.png',
+    'assets/images/properWheel.png'
+  ];
+
+  fins = [
+    'assets/images/aileron.png',
+    'assets/images/aileron2.jpg'
+  ];
+
+
+
   constructor() { }
 
   ngOnInit(): void {
