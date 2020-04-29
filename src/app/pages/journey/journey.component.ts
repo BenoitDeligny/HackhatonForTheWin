5940f748925331c7bee1a68fe0884ff7d1627c02
peacepilou <p-l.b@hotmail.com>
2020-04-29 22:09:28 +0200

    added work of pilou

--- src/app/pages/journey/journey.component.ts
+++ src/app/pages/journey/journey.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DestinationsService } from 'src/app/shared/destinations.service';
+import { Destination } from 'src/app/shared/destination';
 
 @Component({
   selector: 'app-journey',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JourneyComponent implements OnInit {
 
-  constructor() { }
+  berlin: Destination;
+  srilanka: Destination;
+  laponie: Destination;
+  constructor(private destinationService: DestinationsService) { }
 
   ngOnInit(): void {
+    this.destinationService.getBerlin().subscribe(
+      (data) => {this.berlin = data; });
+    this.destinationService. getSriLanka().subscribe(
+      (data) => {this.srilanka = data; });
+    this.destinationService. getLaponie().subscribe(
+      (data) => {this.laponie = data; });
+      }
   }
-
-}
