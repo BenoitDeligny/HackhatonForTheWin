464c2dae587a65757b042ed913304f2a9c7840b5
Couettou <delignyb.pro@gmail.com>
2020-04-30 06:14:16 +0200

    pimp OK

--- src/app/pages/pimp-my-couch/pimp-my-couch.component.ts
+++ src/app/pages/pimp-my-couch/pimp-my-couch.component.ts
@@ -10,36 +10,31 @@ export class PimpMyCouchComponent implements OnInit {
 
   constructor() { }
 
-  // DISPLAY
-  bodyIsDisplay = false;
-  engineIsDisplay = false;
-  wheelIsDisplay = false;
-  finIsDisplay = false;
-
-
   // OPTIONS DU CANAPE
-  couchBodies = [
+  body1Url = 'assets/images/tissuCouch.png';
+  body2Url = 'assets/images/cuirCouch.png';
+  engine1Url = 'assets/images/fastEngine.png';
+  engine2Url = 'assets/images/powerfullEngine.png';
+  wheel1Url = 'assets/images/fastWheel.png';
+  wheel2Url = 'assets/images/fireWheel.png';
+  fin1Url = 'assets/images/aileron.png';
+  fin2Url = 'assets/images/aileron3.png';
+
+  optionsBody = [
     'assets/images/tissuCouch.png',
     'assets/images/cuirCouch.png'
   ];
-
-  engines = [
+  optionsEngine = [
     'assets/images/fastEngine.png',
-    'assets/images/powerfullEngine.png'
+    'assets/images/powerfullEngine.png',
   ];
-
-  wheels = [
+  optionsWheel = [
     'assets/images/fastWheel.png',
     'assets/images/fireWheel.png',
   ];
-
-  fins = [
-    'assets/images/aileron.png',
+  optionFin = [
     'assets/images/aileron3.png'
   ];
-
-  // CANAPE PIMPE
-  finalCouch = [];
 
   // CONSTRUCTION DU CANAPE
   body = '';
@@ -47,50 +42,32 @@ export class PimpMyCouchComponent implements OnInit {
   wheel = '';
   fin = '';
 
-  // DRAG AND DROP
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.couchBodies, event.previousIndex, event.currentIndex);
-    moveItemInArray(this.engines, event.previousIndex, event.currentIndex);
-    moveItemInArray(this.wheels, event.previousIndex, event.currentIndex);
-    moveItemInArray(this.fins, event.previousIndex, event.currentIndex);
-  }
+  // DISPLAY DES IMAGES
+  bodyIsDisplay = false;
+  engineIsDisplay = false;
+  wheelIsDisplay = false;
+  finIsDisplay = false;
 
-  onDrop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex);
-    }
-  }
 
   ngOnInit(): void {
   }
 
-  setbodyIsDisplay() {
+  // METHODS
+  getBodyUrl(url: string) {
+    this.body = url;
     this.bodyIsDisplay = true;
-    this.engineIsDisplay = false;
-    this.finIsDisplay = false;
-    this.wheelIsDisplay = false;
   }
-  setengineIsDisplay() {
-    this.bodyIsDisplay = false;
+  getEngineUrl(url: string) {
+    this.engine = url;
     this.engineIsDisplay = true;
-    this.finIsDisplay = false;
-    this.wheelIsDisplay = false;
   }
-  setwheelIsDisplay() {
-    this.bodyIsDisplay = false;
-    this.engineIsDisplay = false;
+  getWheelUrl(url: string) {
+    this.wheel = url;
     this.wheelIsDisplay = true;
-    this.finIsDisplay = false;
   }
-  setfinIsDisplay() {
-    this.bodyIsDisplay = false;
-    this.engineIsDisplay = false;
-    this.wheelIsDisplay = false;
+  getFinUrl(url: string) {
+    this.fin = url;
     this.finIsDisplay = true;
   }
+
 }
